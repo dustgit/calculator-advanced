a3da960bc43c335367319b23adc10a523ef07b0d
Dustin Starke and Kendra Kelly <dustin@>
2018-08-08 11:16:00 -0700

    add in subtract, multiply, & divide functions and forms

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,37 @@ var multiply = function(number1, number2) {
 var divide = function(number1, number2) {
   return number1 / number2;
 };
+// User interface logic begins here
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#outputadd").text(result);
+	});
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+	$("form#subtract").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#subtract1").val());
+		var number2 = parseInt($("#subtract2").val());
+		var result = subtract(number1, number2);
+		$("#outputsubtract").text(result);
+	});
 
-alert(add(number1, number2));
+	$("form#multiply").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#multiply1").val());
+		var number2 = parseInt($("#multiply2").val());
+		var result = multiply(number1, number2);
+		$("#outputmultiply").text(result);
+	});
+
+	$("form#divide").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#divide1").val());
+		var number2 = parseInt($("#divide2").val());
+		var result = divide(number1, number2);
+		$("#outputdivide").text(result);
+	});
+});
